311cae5383974c6e0e753b436dca8c1616b855e6
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-01-30 01:29:16 -0700

    Created a users and count piece of state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import './App.css';
 
+
+interface User {
+  id: number,
+  username: string,
+}
+
+
 function App() {
+  const [count, setCount] = useState<number | null>(5)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+  useEffect(() => {
+    console.log("mounting")
+
+    return () => console.log("unmounting")
+
+  }, [users])
+
   return (
     <div className="App">
+      <h1> {count} </h1>
+      {/* <button onClick={() => setCount(prev => prev + 1)}> Add </button> */}
     </div>
   );
 }
