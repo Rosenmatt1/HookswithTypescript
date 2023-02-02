46ee4b859301bead2e015cda079a71f35f30d02a
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-02-02 02:31:09 -0700

    Created a useCallback function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useCallback } from 'react';
 import './App.css';
 
+//useCallback Memoizes a function so it's not always recreated
+
 
 interface User {
   id: number,
@@ -9,20 +11,21 @@ interface User {
 
 
 function App() {
-  const [count, setCount] = useState<number | null>(5)
+  const [count, setCount] = useState<number>(5)
   const [users, setUsers] = useState<User[] | null>(null)
 
   useEffect(() => {
     console.log("mounting")
 
     return () => console.log("unmounting")
-
   }, [users])
+
+ const addTwo = useCallback(() => setCount(prev => prev +1 ), [])
 
   return (
     <div className="App">
       <h1> {count} </h1>
-      {/* <button onClick={() => setCount(prev => prev + 1)}> Add </button> */}
+      <button onClick={() => setCount(prev => prev + 1)}> Add </button>
     </div>
   );
 }
